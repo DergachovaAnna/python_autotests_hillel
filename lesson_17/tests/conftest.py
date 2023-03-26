44f787bdbcc0f93618ab2195cf512d038b454fed
DergachovaAnna <ann.dergachova@gmail.com>
2023-03-26 19:12:09 +0200

    fixed the code

--- lesson_17/tests/conftest.py
+++ lesson_17/tests/conftest.py
@@ -3,10 +3,20 @@ from lesson_17.Human.human import Human
 
 
 @pytest.fixture
-def create_human_positive():
+def create_human_with_valid_age():
     return Human("Alice", 16, 'female')
 
 
 @pytest.fixture
-def create_human_negative():
+def create_human_with_invalid_age():
     return Human("Alice", 100, 'female')
+
+
+@pytest.fixture(params=[('Ann', 1, "female"), ('Bob', 99, "male")])
+def human_data(request):
+    return request.param
+
+
+@pytest.fixture(params=[('Ann', 1, "female"), ('Bob', 98, "male")])
+def human_age_boundary_values(request):
+    return request.param
--- lesson_17/tests/test_human.py
+++ lesson_17/tests/test_human.py
@@ -2,44 +2,45 @@ import pytest
 from lesson_17.Human.human import Human
 
 
-@pytest.mark.parametrize("name,age,gender", [('Ann', 1, "female"), ('Bob', 99, "male")])
 @pytest.mark.positive
-def test_create_human(name, age, gender):
+def test_create_human(human_data):
+    name, age, gender = human_data
     human = Human(name, age, gender)
     assert hasattr(human, '_Human__status')
     assert hasattr(human, '_Human__age_limit')
 
 
 @pytest.mark.positive
-def test_change_gender(create_human_positive):
+def test_change_gender(create_human_with_valid_age):
     # Change to a different gender
-    new_gender = "male" if create_human_positive.gender == "female" else "female"
-    create_human_positive.change_gender(new_gender)
-    assert create_human_positive.gender == new_gender
+    new_gender = "male" if create_human_with_valid_age.gender == "female" else "female"
+    create_human_with_valid_age.change_gender(new_gender)
+    assert create_human_with_valid_age.gender == new_gender
 
 
 @pytest.mark.negative
-def test_change_to_existed_gender(create_human_positive):
+def test_change_to_existed_gender(create_human_with_valid_age):
     # Try to change to the same gender
-    new_gender = "male" if create_human_positive.gender == "male" else "female"
+    new_gender = "male" if create_human_with_valid_age.gender == "male" else "female"
     with pytest.raises(Exception):
-        create_human_positive.change_gender(new_gender)
+        create_human_with_valid_age.change_gender(new_gender)
 
 
 @pytest.mark.positive
-def test_grow_human_below_limit(create_human_positive):
-    for number in range(create_human_positive.age, 99):
-        initial_age = create_human_positive.age
-        create_human_positive.grow()
-        assert create_human_positive.age == initial_age + 1
-        assert create_human_positive._Human__status == "alive"  # below age limit
+def test_grow_human_below_limit(human_age_boundary_values):
+    name, age, gender = human_age_boundary_values
+    human = Human(name, age, gender)
+    initial_age = human.age
+    human.grow()
+    assert human.age == initial_age + 1
+    assert human._Human__status == "alive"  # below age limit
 
 
 @pytest.mark.negative
-def test_grow_human_above_limit(create_human_negative):
-    assert create_human_negative.age == 100
-    create_human_negative.grow()  # above age limit
-    assert create_human_negative._Human__status == "dead"
+def test_grow_human_above_limit(create_human_with_invalid_age):
+    assert create_human_with_invalid_age.age == 100
+    create_human_with_invalid_age.grow()  # above age limit
+    assert create_human_with_invalid_age._Human__status == "dead"
 
 
 @pytest.mark.negative
@@ -58,14 +59,14 @@ def test_grow_human_age_below_zero():
 
 
 @pytest.mark.positive
-def test_human_is_alive(create_human_positive):
-    assert create_human_positive._Human__is_alive() is True
+def test_human_is_alive(create_human_with_valid_age):
+    assert create_human_with_valid_age._Human__is_alive() is True
 
 
 @pytest.mark.negative
-def test_create_human_invalid_age(create_human_negative):
+def test_create_human_invalid_age(create_human_with_invalid_age):
     # Check that age affects whether human is created, alive or not
-    assert create_human_negative._Human__status is 'dead'
+    assert create_human_with_invalid_age._Human__status is 'dead'
 
 
 @pytest.mark.parametrize("name,age,gender", [('Ann', 0, "female"), ('Bob', -1, "male")])
