e8eacb2dea3e9241222524d306b2bd03dd4196c0
DergachovaAnna <69525074+DergachovaAnna@users.noreply.github.com>
2023-05-07 02:11:44 +0200

    Update test_matches_page.py

--- lesson_19/all_tests/tests_ui/test_matches_page.py
+++ lesson_19/all_tests/tests_ui/test_matches_page.py
@@ -2,7 +2,6 @@ import pytest
 
 
 @pytest.mark.regression
-@pytest.mark.ci
 def test_add_match(open_add_player_page):
     created_match = open_add_player_page.add_player_with_required_fields().click_matches_button().create_match()
     assert created_match.find_last_added_match().is_displayed(), "Added match is not found in Matches table"
